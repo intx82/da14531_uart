e2f340eef8a04fa555e9aca40445ca7cebbad81a
Danil Ruban <druban@sequans.com>
2022-09-03 02:25:30 +0300

    python gatt cmd utillity

--- readme.md
+++ readme.md
@@ -1,15 +1,10 @@
-# Переходник BLE - UART (DA14585)
-
-![uart.gif](https://github.com/intx82/da14531_uart/raw/master/doc/uart.gif)
+# Modbus over BLE
 
 ## Схема
 
 В переходнике используется модуль PAN1740A на базе контроллера DA14585
 <https://na.industrial.panasonic.com/products/wireless-connectivity/bluetooth/lineup/bluetooth-low-energy/series/133809>
 
-* Выводы 5,6 - Uart. Детальнее см.даташит в <https://github.com/intx82/da14531_uart/raw/master/doc/247156_fileversion.pdf>
-  * P0.5 (5) - RX
-  * P0.4 (6) - TX
 * Выводы 16,18 - SWD
 * Вывод 8 - VCC
 * Вывод A,B,C,D,15,17,19, 7 - GND
@@ -17,16 +12,7 @@
 
 ## Прошивка
 
-Прошивка эмулирует поведение nordic uart:
-
-<https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.4.0/nrf/include/bluetooth/services/nus.html>
-<https://learn.adafruit.com/introducing-the-adafruit-bluefruit-le-uart-friend/uart-service>
-
-Скорость порта 115200.
-
-Настройки порта см. файлы <src/user_peripherial.c> / <src/user_peripherial.h>
-
-Настройки BLE профиля: <src/profile/uart_profile.c> / <src/profile/uart_profile.h>
+Прошивка эмулирует поведение dialog dsps
 
 ## Сборка
 
@@ -35,3 +21,63 @@
 Перед стартом проекта запустите bootstrap.sh который подготовит SDK, после проект можно собрать через src/ble_uart.uvprojx
 
 Выходные файлы в папке src/out
+
+## Тесты
+
+Тест собирается через makefile:
+
+```
+make -C tests
+./tests/out/test
+```
+
+## Протокол
+
+Протокол общения описан в [doc/protocol.md](https://github.com/intx82/da14531_uart/raw/cmd_mux/doc/protocol.md)
+
+## Утилита для выполнения команд
+
+Для запуска утилиты должна быть установленн BLE_GATT в соотвтествии с описанием [https://github.com/ukBaz/BLE_GATT](https://github.com/ukBaz/BLE_GATT)
+
+```
+$ python3 -m venv venv_ble
+$ . venv_ble/bin/activate
+$ pip3 install BLE_GATT
+```
+
+Утилита запускается с следующими аргументами:
+
+```
+python3 venv_ble/dsps_tool.py [ DEV_MAC ] [ CMD ] [ ARGS... ]
+```
+
+### Пример чтения регистра
+```
+python3 venv_ble/dsps_tool.py 00:13:43:A3:6F:56 INT_RD 03 02
+```
+
+Команда выполняет чтения регистров 03,04 (начиная с 3-го, два регистра)
+
+### Пример команды записи блока
+```
+python3 venv_ble/dsps_tool.py 00:13:43:A3:6F:56 blob_wr 12aa55 0102030405060708090a0b0c0d0e0f00
+```
+
+Запись блока по адресу 0x12aa55 c значением 0102030405060708090a0b0c0d0e0f00
+
+### Пример Чтение блока
+
+```
+python3 venv_ble/dsps_tool.py 00:13:43:A3:6F:56 blob_rd 12aa55
+```
+
+Читает блок по адресу 0x12aa55
+
+### Пример записи регистра
+
+```
+python3 venv_ble/dsps_tool.py 00:13:43:A3:6F:56 int_wr 3 2
+```
+
+Записывает регистры значением 2
+
---
+++ tool/venv_ble/dsps_tool.py
@@ -0,0 +1,122 @@
+import sys
+import struct
+import BLE_GATT
+from gi.repository import GLib
+
+
+uart_rx = '0783b03e-8535-b5a0-7140-a304d2495cba'
+uart_tx = '0783b03e-8535-b5a0-7140-a304d2495cb8'
+
+
+class cmd_t:
+    def __init__(self, cmd: int, arg: bytes) -> None:
+        self.cmd = cmd
+        self.arg = arg
+        self.error = 0
+
+    def serialize(self) -> bytes:
+        return struct.pack(f'c{len(self.arg)}s', self.cmd.to_bytes(1, 'little'), self.arg)
+
+    def deserialize(self, inp: bytes) -> None:
+        (cmd, self.arg) = struct.unpack(f'c{len(inp)-1}s', inp)
+        self.cmd = cmd[0]
+        if self.cmd & 0x80:
+            self.error = self.arg[0]
+            if self.error != 0xfb:  # -5
+                raise IOError(
+                    f'Got MODBUS Exception [ cmd: {self.cmd & 0x7f}, error: {self.error} ]', self.error)
+
+
+class int_rd(cmd_t):
+    def __init__(self, var: int = 0, count: int = 0) -> None:
+        super().__init__(3, struct.pack('>HH', int(var), int(count)))
+        self.count = count
+        self.var = var
+        self.values = []
+
+    def deserialize(self, inp: bytes):
+        super().deserialize(inp)
+        self.count = self.arg[0] / 2
+        self.values = list(struct.unpack('<h'*self.count, bytes(self.arg[1:])))
+
+    def __repr__(self) -> str:
+        return f'int_rd: {{ var: {self.var}, count: {self.count}, values: {self.values}}}'
+
+
+class int_wr(cmd_t):
+    def __init__(self, var: int, val: int) -> None:
+        super().__init__(6, struct.pack('>HH', int(var), int(val)))
+        self.val = val
+        self.var = var
+
+    def __repr__(self) -> str:
+        return f'int_wr: {{ var: {self.var}, val: {self.val}}}'
+
+
+class blob_rd(cmd_t):
+    def __init__(self, addr: int) -> None:
+        addr = int(addr, 16)
+        super().__init__(0x12, struct.pack('ccc', (addr & 0xff).to_bytes(1, 'big'),
+                                           ((addr & 0xff00) >> 8).to_bytes(1, 'big'), (addr >> 16).to_bytes(1, 'big')))
+        self.addr = addr
+        self.data = bytes()
+
+    def __repr__(self) -> str:
+        return f'blob_rd: {{ addr: {self.addr}, data: {self.data.hex()}}}'
+
+    def deserialize(self, inp: bytes):
+        super().deserialize(inp)
+        self.data = bytes(self.arg[3:])
+
+
+class blob_wr(cmd_t):
+    def __init__(self, addr: int, data: bytes) -> None:
+        addr = int(addr, 16)
+        if isinstance(data, str):
+            data = bytes.fromhex(data)
+
+        super().__init__(0x13, struct.pack('ccc16s', (addr & 0xff).to_bytes(1, 'big'),
+                                           ((addr & 0xff00) >> 8).to_bytes(
+                                               1, 'big'), (addr >> 16).to_bytes(1, 'big'), data))
+        self.addr = addr
+        self.data = data
+
+    def __repr__(self) -> str:
+        return f'blob_wr: {{ addr: {self.addr}, data: {self.data.hex}}}'
+
+
+def notify_handler(value):
+    value = bytes(value)
+    print(f"Received: {value.hex()}")
+    try:
+        cmd.deserialize(bytes(value))
+        print(f'recv: {cmd}')
+    except IOError as e:
+        print(e)
+
+    dev.cleanup()
+
+
+def send(out: bytes):
+    dev.char_write(uart_rx, out)
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 3:
+        dev = BLE_GATT.Central(sys.argv[1])
+        print(f'Connecting to: {sys.argv[1]}')
+        dev.connect()
+        print('Connected')
+
+        dev.on_value_change(uart_tx, notify_handler)
+        cmd = globals()[sys.argv[2].lower()](*sys.argv[3:])
+        try:
+
+            print(f'Send: {cmd} : {cmd.serialize().hex()}')
+            send(cmd.serialize())
+            # GLib.timeout_add_seconds(5, send_ping)
+
+            dev.wait_for_notifications()
+        finally:
+            dev.disconnect()
